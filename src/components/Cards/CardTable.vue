c58e038e5d23acf7e46ae2caaea02cf348792d23
OrcaguDev <ocastillogs@ucvvirtual.edu.pe>
2023-10-08 18:53:35 -0500

    comit56

--- src/components/Cards/CardTable.vue
+++ src/components/Cards/CardTable.vue
@@ -1,15 +1,10 @@
 <template>
-  <div
-    class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded"
-    :class="[color === 'light' ? 'bg-white' : 'bg-emerald-900 text-white']"
-  >
+  <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded"
+    :class="[color === 'light' ? 'bg-white' : 'bg-emerald-900 text-white']">
     <div class="rounded-t mb-0 px-4 py-3 border-0">
       <div class="flex flex-wrap items-center">
         <div class="relative w-full px-4 max-w-full flex-grow flex-1">
-          <h3
-            class="font-semibold text-lg"
-            :class="[color === 'light' ? 'text-blueGray-700' : 'text-white']"
-          >
+          <h3 class="font-semibold text-lg" :class="[color === 'light' ? 'text-blueGray-700' : 'text-white']">
             Tabla de Eventos
           </h3>
         </div>
@@ -26,8 +21,7 @@
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >
+              ]">
               #
             </th>
             <th
@@ -36,8 +30,7 @@
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >
+              ]">
               TITULO
             </th>
             <th
@@ -46,8 +39,7 @@
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >
+              ]">
               EXPOSITOR
             </th>
             <th
@@ -56,8 +48,7 @@
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >
+              ]">
               LUGAR
             </th>
             <th
@@ -66,8 +57,7 @@
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >
+              ]">
               FECHA Y HORA
             </th>
             <th
@@ -76,115 +66,102 @@
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >AFORO TOTAL</th>
+              ]">AFORO TOTAL</th>
             <th
               class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
               :class="[
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >BUTACAS RESERVADAS</th>
+              ]">BUTACAS RESERVADAS</th>
             <th
               class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
               :class="[
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >AFORO DISPONIBLE</th>            
-          <th
+              ]">AFORO DISPONIBLE</th>
+            <th
               class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-center"
               :class="[
                 color === 'light'
                   ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
                   : 'bg-emerald-800 text-emerald-300 border-emerald-700',
-              ]"
-            >Acciones</th>
+              ]">Acciones</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(item, index) in eventos" :key="index">
-            <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-              {{index+1}}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ index + 1 }}
             </td>
-            <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-              {{item.nombre}}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ item.nombre }}
             </td>
-             <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-              {{item.expositor}}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ item.expositor }}
             </td>
-             <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-              {{item.lugar}}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ item.lugar }}
             </td>
-             <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-              {{item.fechaInicio}}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ item.fechaInicio }}
             </td>
-             <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-            {{item.aforo_total}}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ item.aforo_total }}
             </td>
-            <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
               {{ item.butacas_reservadas }}
             </td>
-            <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-            >
-              {{item.aforo_total - item.butacas_reservadas }}
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+              {{ item.aforo_total - item.butacas_reservadas }}
             </td>
             <td
-              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 flex justify-center"
-            >
+              class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 flex justify-center">
 
-            <!-- <router-link to="'/admin/detalleEvento/'+item.id_evento"> -->
-            <router-link :to="'/admin/detalleEvento/'+item.id_evento">
-              <button class="bg-indigo-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                <i class="fas fa-list"></i>
+              <!-- Boton de detalle -->
+              <router-link :to="'/admin/detalleEvento/' + item.id_evento">
+                <button
+                  class="bg-indigo-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button">
+                  <i class="fas fa-list"></i>
+                </button>
+              </router-link>
+
+              <!-- Boton de eliminar -->
+              <button @click="eliminarEvento(evento.codigo)"
+                class="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button">
+                <i class="fas fa-trash"></i>
               </button>
-            </router-link>
-            <button class="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-              <i class="fas fa-trash"></i>
-            </button>
-            
-            <!-- <router-link to="'/admin/editar/editEvento/'+item.id_regla"> -->
-            <router-link :to="'/admin/editar/editEvento/'+item.id_evento">         
-              <button class="bg-yellow-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                <i class="fas fa-edit"></i>
-              </button>
-            </router-link>
+
+              <!-- Boton de editar -->
+              <router-link :to="'/admin/editar/editEvento/' + item.id_evento">
+                <button
+                  class="bg-yellow-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button">
+                  <i class="fas fa-edit"></i>
+                </button>
+              </router-link>
 
 
-            <!-- Boton de inscripcion -->
-            <router-link :to="'/inscripciones/inscripcion/' + item.id_evento" target="_blank">
-            <button class="bg-emerald-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
-            type="button"
-            >
-            <i class="fas fa-id-card"></i>
-            </button>
-            </router-link>
+              <!-- Boton de inscripcion -->
+              <router-link :to="'/inscripciones/inscripcion/' + item.id_evento" target="_blank">
+                <button
+                  class="bg-emerald-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button">
+                  <i class="fas fa-id-card"></i>
+                </button>
+              </router-link>
 
-            <!-- Boton de inscripcion del Publico -->
-            <router-link :to="'/inscripciones/inscripcionPublica/' + item.id_evento" target="_blank">
-            <button class="bg-black text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
-            type="button"
-            >
-            <i class="fas fa-globe"></i>
-            </button>
-            </router-link>
+              <!-- Boton de inscripcion del Publico -->
+              <router-link :to="'/inscripciones/inscripcionPublica/' + item.id_evento" target="_blank">
+                <button
+                  class="bg-black text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button">
+                  <i class="fas fa-globe"></i>
+                </button>
+              </router-link>
 
             </td>
           </tr>
@@ -201,43 +178,61 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      eventos:[],
-      apii:{
-        api_token:''
+      eventos: [],
+      apii: {
+        api_token: ''
       },
-      showModal:false
+      evento: {
+        api_token: '',
+        codigo: ''
+      },
+      showModal: false
     };
   },
   components: {
-    // TableDropdown,
   },
   props: {
     color: {
       default: "light",
       validator: function (value) {
-        // The value must match one of these strings
         return ["light", "dark"].indexOf(value) !== -1;
       },
     },
   },
-  methods:{
-     getTotal(){
-        let objetoString = localStorage.getItem("token");
-        let objeto = JSON.parse(objetoString);
-        this.apii.api_token=objeto;
-        const auth = {
-          headers: {'Content-Type': 'application/json'} 
-        }
-        axios.post('http://localhost:8000/eventos',this.apii,auth).then(({data}) => {
-            this.eventos = data;
-        }).catch((error) => {
-            console.log(error);
-        });
+  methods: {
 
+    //Funcion de obtener Eventos
+    getTotal() {
+      let objetoString = localStorage.getItem("token");
+      let objeto = JSON.parse(objetoString);
+      this.apii.api_token = objeto;
+      const auth = {
+        headers: { 'Content-Type': 'application/json' }
+      }
+      axios.post('http://localhost:8000/eventos', this.apii, auth).then(({ data }) => {
+        this.eventos = data;
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+
+
+    //Funcion de eliminar Evento
+    eliminarEvento(id) {
+      let objetoString = localStorage.getItem("token");
+      let objeto = JSON.parse(objetoString);
+      this.evento.api_token = objeto;
+      this.evento.codigo = id;
+      const auth = {
+        headers: { 'Content-Type': 'application/json' }
+      }
+      axios.post('http://localhost:8000/deleteEvento', this.evento, auth).then(() => {
+        this.getTotal();
+      });
     }
   },
-   created(){
-        this.getTotal();
-   }
+  created() {
+    this.getTotal();
+  }
 };
 </script>
