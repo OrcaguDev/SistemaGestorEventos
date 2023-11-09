1a201893bff61329b30ff8d3420e38750e33566a
OrcaguDev <ocastillogs@ucvvirtual.edu.pe>
2023-11-09 00:31:21 -0500

    PAgos

---
+++ src/components/Cards/CardListaPagos.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded"
+        :class="[color === 'light' ? 'bg-white' : 'bg-emerald-900 text-white']">
+        <div class="rounded-t mb-0 px-4 py-3 border-0">
+            <div class="flex flex-wrap items-center">
+                <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                    <h3 class="font-semibold text-lg" :class="[color === 'light' ? 'text-blueGray-700' : 'text-white']">
+                        Lista de URL - PAGOS
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="block w-full overflow-x-auto">
+            <!-- Projects table -->
+            <table class="items-center w-full bg-transparent ">
+                <thead>
+                    <tr>
+                        <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                            :class="[
+                                color === 'light'
+                                    ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                                    : 'bg-emerald-800 text-emerald-300 border-emerald-700',
+                            ]">
+                            #
+                        </th>
+                        <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                            :class="[
+                                color === 'light'
+                                    ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                                    : 'bg-emerald-800 text-emerald-300 border-emerald-700',
+                            ]">
+                            Evento
+                        </th>
+                        <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                            :class="[
+                                color === 'light'
+                                    ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                                    : 'bg-emerald-800 text-emerald-300 border-emerald-700',
+                            ]">
+                            descripcion
+                        </th>
+                        <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                            :class="[
+                                color === 'light'
+                                    ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                                    : 'bg-emerald-800 text-emerald-300 border-emerald-700',
+                            ]">
+                            link de pago
+                        </th>
+                        <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                            :class="[
+                                color === 'light'
+                                    ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                                    : 'bg-emerald-800 text-emerald-300 border-emerald-700',
+                            ]">
+                            estado
+                        </th>
+                        <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                            :class="[
+                                color === 'light'
+                                    ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                                    : 'bg-emerald-800 text-emerald-300 border-emerald-700',
+                            ]">
+                            Acciones
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(regla, index) in reglas" :key="index">
+                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {{ index + 1 }}
+                        </td>
+
+                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {{ regla.nombre }}
+                        </td>
+                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {{ regla.descripcion }}
+                        </td>
+                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {{ regla.url }}
+                        </td>
+                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            Activo
+                        </td>
+                        <td
+                            class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 flex justify-center">
+                            <button @click="eliminarRegla()"
+                                class="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button">
+                                <i class="fas fa-trash"></i>
+                            </button>
+
+                            <router-link :to="'/admin/editar/editReglas/' + regla.id_regla">
+                                <button
+                                    class="bg-yellow-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button">
+                                    <i class="fas fa-edit"></i>
+                                </button>
+                            </router-link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            reglas: [],
+            apii: {
+                api_token: ''
+            },
+            regla: {
+                api_token: '',
+                codigo: ''
+            }
+        }
+    },
+    methods: {
+        getTotal() {
+            let objetoString = localStorage.getItem("token");
+            let objeto = JSON.parse(objetoString);
+            this.apii.api_token = objeto;
+            const auth = {
+                headers: { 'Content-Type': 'application/json' }
+            }
+            axios.post('http://localhost:8000/reglas', this.apii, auth).then(({ data }) => {
+                this.reglas = data;
+                // console.log(data);
+            }).catch((error) => {
+                console.log(error);
+            });
+        },
+        eliminarRegla() {
+            console.log(this.regla.id_regla);
+        }
+    },
+    created() {
+        this.getTotal();
+    },
+    props: {
+        color: {
+            default: "light",
+            validator: function (value) {
+                // The value must match one of these strings
+                return ["light", "dark"].indexOf(value) !== -1;
+            },
+        },
+    },
+};
+</script>
