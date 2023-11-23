2a8885bd7c1ea383874e62e38931d996398ef633
NandoSan <luis27.2000.Fernando@gmail.com>
2023-11-23 02:07:24 -0500

    correciones vista

--- src/components/Cards/CardEvento.vue
+++ src/components/Cards/CardEvento.vue
@@ -1,44 +1,44 @@
 <template>
   <div
-    class="relative flex flex-col min-w-0 break-words bg-white w-full xl:w-4/12 mb-6 shadow-xl rounded-lg mt-16"
+    class="relative flex flex-col w-full min-w-0 mt-16 mb-6 break-words bg-white rounded-lg shadow-xl xl:w-4/12"
   >
     <div class="px-6">
       <div class="flex flex-wrap justify-center">
-        <div class="w-full px-4 flex justify-center">
+        <div class="flex justify-center w-full px-4">
           <div class="relative">
             <img
               alt="..."
               :src="team2"
-              class="shadow-xl h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+              class="absolute h-auto -m-16 -ml-20 align-middle border-none shadow-xl lg:-ml-16 max-w-150-px"
             />
           </div>
         </div>
-        <div class="w-full px-4 text-center mt-20">
-          <div class="flex justify-center py-4 lg:pt-4 pt-8">
+        <div class="w-full px-4 mt-20 text-center">
+          <div class="flex justify-center py-4 pt-8 lg:pt-4">
           </div>
         </div>
       </div>
-      <div class="text-center mt-12">
+      <div class="mt-12 text-center">
         <h3
-          class="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
+          class="mb-2 text-xl font-semibold leading-normal text-blueGray-700"
         >
           Titulo del Evento
         </h3>
         <div
-          class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase"
+          class="mt-0 mb-2 text-sm font-bold leading-normal uppercase text-blueGray-400"
         >
-          <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+          <i class="mr-2 text-lg fas fa-map-marker-alt text-blueGray-400"></i>
           Lugar, fecha
         </div>
-        <div class="mb-2 text-blueGray-600 mt-10">
-          <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
+        <div class="mt-10 mb-2 text-blueGray-600">
+          <i class="mr-2 text-lg fas fa-briefcase text-blueGray-400"></i>
           Nombre del Expositor
         </div>
 
       </div>
-      <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+      <div class="py-10 mt-10 text-center border-t border-blueGray-200">
         <div class="flex flex-wrap justify-center">
-          <div class="w-full lg:w-9/12 px-4">
+          <div class="w-full px-4 lg:w-9/12">
             <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
               Descripcion del evento
             </p>
