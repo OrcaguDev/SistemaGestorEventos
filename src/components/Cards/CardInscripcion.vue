3d7003a1103b1e8724b9bf9011c1da7ce838426d
NandoSan <luis27.2000.Fernando@gmail.com>
2023-12-27 22:04:48 -0500

    aa

--- src/components/Cards/CardInscripcion.vue
+++ src/components/Cards/CardInscripcion.vue
@@ -203,100 +203,99 @@ import Swal from 'sweetalert2'
 import Main from '../../main.js'
 
 export default {
-  data() {
-    return {
-      evento: {
-        nombre: '',
-        expositor: '',
-        lugar: '',
-        fechaInicio: '',
-        descripcion: '',
-        img: '',
-        informe: '',
-        fechaInscripcion: '',
-        fechaInscripcionFin: ''
-      },
-      inscripcion: {
-        dni: '',
-        nombre: '',
-        apellido: '',
-        celular: '',
-        email: '',
-        certificacion: false,
-        habilidad: '',
-        url_id: '',
-        cod: ''
-      },
-      isVisiblee: 0,
-      isVisible: 0,
-      url_id: '',
-      mensaje: '',
-      isVisibleeee: 1,
-      alert: '',
-      valoor: Main.url,
-      validacion: '',
-      BtnInscripcion: 1
-    }
-  },
-  mounted() {
-    this.url_id = this.$route.params.id
-    this.getEvento(this.url_id)
-  },
-  methods: {
-    async validarDni() {
-      const valor = Main.url;
-      this.inscripcion.url_id = this.$route.params.id;
-      axios.post(`${valor}/obtenerReglaEvento/${this.inscripcion.url_id}`).then((data) => {
-        const url_validacion = data.data[0].url;
-        axios.get(`${url_validacion}/${this.inscripcion.cod}`).then((data) => {
-          const validacion = data.data.data.habilidad.var_habilitacion_estado;
-          this.validacion = validacion;
-          if (this.validacion === 'Habilitado') {
-
-            axios.get(`https://app-cipcdll.com:81/obtener_persona_datos_xterceros/${this.inscripcion.cod}`).then((data) => {
-              this.inscripcion.dni = data.data.data.dni;
-              this.inscripcion.nombre = data.data.data.nombres;
-              this.inscripcion.apellido = data.data.data.paterno + ' ' + data.data.data.materno;
-              this.inscripcion.celular = data.data.data.celular;
-              this.inscripcion.email = data.data.data.email;
-              this.isVisiblee = 1;
-            });
-            this.AlertSwall('Correcto!!', 'Te encuentras habilitado para este evento.', 'success');
-          } else {
-            this.inscripcion.nombre = '';
-            this.inscripcion.apellido = '';
-            this.inscripcion.celular = '';
-            this.inscripcion.email = '';
-            this.AlertSwall('Error!!', 'No se encuentra habilitado para este evento.', 'error');
-          }
-        }).catch((error) => {
-          console.log(error);
-        });
-      })
+    data () {
+        return {
+            evento: {
+                nombre: '',
+                expositor: '',
+                lugar: '',
+                fechaInicio: '',
+                descripcion: '',
+                img: '',
+                informe: '',
+                fechaInscripcion: '',
+                fechaInscripcionFin: ''
+            },
+            inscripcion: {
+                dni: '',
+                nombre: '',
+                apellido: '',
+                celular: '',
+                email: '',
+                certificacion: false,
+                habilidad: '',
+                url_id: '',
+                cod: ''
+            },
+            isVisiblee: 0,
+            isVisible: 0,
+            url_id: '',
+            mensaje: '',
+            isVisibleeee: 1,
+            alert: '',
+            valoor: Main.url,
+            validacion: '',
+            BtnInscripcion: 1
+        }
     },
-    getEvento(id) {
-      const valor = Main.url
-      const auth = {
-        headers: { 'Content-Type': 'application/json' }
-      }
-      axios.post(`${valor}/evento/${id}`, auth).then(({ data }) => {
-        this.evento.nombre = data[0].nombre
-        this.evento.expositor = data[0].expositor
-        this.evento.lugar = data[0].lugar
-        this.evento.fechaInicio = data[0].fechaInicio
-        this.evento.descripcion = data[0].descripcion
-        this.evento.aforo_total = data[0].aforo_total
-        this.evento.butacas_reservadas = data[0].butacas_reservadas
-        this.evento.fechaFin = data[0].fechaFin
-        this.evento.id_regla = data[0].id_regla
-        this.evento.fechaInscripcion = data[0].fechaInscripcion
-        this.evento.fechaInscripcionFin = data[0].fechaInscripcionFin
-        this.evento.img = data[0].img
-        this.evento.informe = data[0].informe
-      }).catch((error) => {
-        console.log(error)
-      })
+    mounted () {
+        this.url_id = this.$route.params.id
+        this.getEvento(this.url_id)
     },
+    methods: {
+        async validarDni () {
+            const valor = Main.url
+            this.inscripcion.url_id = this.$route.params.id
+            axios.post(`${valor}/obtenerReglaEvento/${this.inscripcion.url_id}`).then((data) => {
+                const url_validacion = data.data[0].url
+                axios.get(`${url_validacion}/${this.inscripcion.cod}`).then((data) => {
+                    const validacion = data.data.data.habilidad.var_habilitacion_estado
+                    this.validacion = validacion
+                    if (this.validacion === 'Habilitado') {
+                        axios.get(`https://app-cipcdll.com:81/obtener_persona_datos_xterceros/${this.inscripcion.cod}`).then((data) => {
+                            this.inscripcion.dni = data.data.data.dni
+                            this.inscripcion.nombre = data.data.data.nombres
+                            this.inscripcion.apellido = data.data.data.paterno + ' ' + data.data.data.materno
+                            this.inscripcion.celular = data.data.data.celular
+                            this.inscripcion.email = data.data.data.email
+                            this.isVisiblee = 1
+                        })
+                        this.AlertSwall('Correcto!!', 'Te encuentras habilitado para este evento.', 'success')
+                    } else {
+                        this.inscripcion.nombre = ''
+                        this.inscripcion.apellido = ''
+                        this.inscripcion.celular = ''
+                        this.inscripcion.email = ''
+                        this.AlertSwall('Error!!', 'No se encuentra habilitado para este evento.', 'error')
+                    }
+                }).catch((error) => {
+                    console.log(error)
+                })
+            })
+        },
+        getEvento (id) {
+            const valor = Main.url
+            const auth = {
+                headers: { 'Content-Type': 'application/json' }
+            }
+            axios.post(`${valor}/evento/${id}`, auth).then(({ data }) => {
+                this.evento.nombre = data[0].nombre
+                this.evento.expositor = data[0].expositor
+                this.evento.lugar = data[0].lugar
+                this.evento.fechaInicio = data[0].fechaInicio
+                this.evento.descripcion = data[0].descripcion
+                this.evento.aforo_total = data[0].aforo_total
+                this.evento.butacas_reservadas = data[0].butacas_reservadas
+                this.evento.fechaFin = data[0].fechaFin
+                this.evento.id_regla = data[0].id_regla
+                this.evento.fechaInscripcion = data[0].fechaInscripcion
+                this.evento.fechaInscripcionFin = data[0].fechaInscripcionFin
+                this.evento.img = data[0].img
+                this.evento.informe = data[0].informe
+            }).catch((error) => {
+                console.log(error)
+            })
+        },
 
         storeInscripcion () {
             const valor = Main.url
@@ -312,41 +311,41 @@ export default {
             const día = fechaActual.getDate()
             const fechaFormateada = `${año}-${mes < 10 ? '0' : ''}${mes}-${día < 10 ? '0' : ''}${día}`
 
-      if (fechaFormateada >= fechaInscripcion && fechaFormateada <= fechaInscripcionFin) {
-        const auth = {
-          headers: { 'Content-Type': 'application/json' }
-        }
-        // eslint-disable-next-line camelcase
-        const url_combinado = `${valor}/validateInscripciones?dni=${dni}&id_evento=${this.inscripcion.url_id}`
-        axios.post(url_combinado, this.inscripcion, auth).then((data) => {
-          if (data.data[0].cuentaInscripcion > 0) {
-            this.AlertSwall('Error!!', 'Ya se encuentra registrado a este evento', 'error')
-          } else {
-            axios.post(`${valor}/storeInscripcion`, this.inscripcion, auth).then((data) => {
-              this.AlertSwall('Registrado', 'Registo completado satisfactoriamente!', 'success')
-              // eslint-disable-next-line no-unused-expressions
-              this.isVisibleeee === 0
-              this.inscripcion.cod = ''
-              this.inscripcion.nombre = ''
-              this.inscripcion.apellido = ''
-              this.inscripcion.celular = ''
-              this.inscripcion.email = ''
-              this.inscripcion.certificacion = false
-            this.BtnInscripcion = 1
+            if (fechaFormateada >= fechaInscripcion && fechaFormateada <= fechaInscripcionFin) {
+                const auth = {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+                // eslint-disable-next-line camelcase
+                const url_combinado = `${valor}/validateInscripciones?dni=${dni}&id_evento=${this.inscripcion.url_id}`
+                axios.post(url_combinado, this.inscripcion, auth).then((data) => {
+                    if (data.data[0].cuentaInscripcion > 0) {
+                        this.AlertSwall('Error!!', 'Ya se encuentra registrado a este evento', 'error')
+                    } else {
+                        axios.post(`${valor}/storeInscripcion`, this.inscripcion, auth).then((data) => {
+                            this.AlertSwall('Registrado', 'Registo completado satisfactoriamente!', 'success')
+                            // eslint-disable-next-line no-unused-expressions
+                            this.isVisibleeee === 0
+                            this.inscripcion.cod = ''
+                            this.inscripcion.nombre = ''
+                            this.inscripcion.apellido = ''
+                            this.inscripcion.celular = ''
+                            this.inscripcion.email = ''
+                            this.inscripcion.certificacion = false
+                            this.BtnInscripcion = 1
+                        })
+                    }
+                })
+            } else {
+                this.AlertSwall('Error!!', 'El evento ya no se encuentra disponible.', 'error')
+            }
+        },
+        AlertSwall ($title, $text, $icon) {
+            Swal.fire({
+                title: $title,
+                text: $text,
+                icon: $icon
             })
-          }
-        })
-      } else {
-        this.AlertSwall('Error!!', 'El evento ya no se encuentra disponible.', 'error')
-      }
-    },
-    AlertSwall($title, $text, $icon) {
-      Swal.fire({
-        title: $title,
-        text: $text,
-        icon: $icon
-      })
-    }
+        }
 
     }
 }
